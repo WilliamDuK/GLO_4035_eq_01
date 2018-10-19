684cbab04cd8a591c4fea59b81f4b87f61467ca8
WilliamDuK <william.duquet@videotron.ca>
2018-10-19 12:18:50 -0400

    Changed host to localhost

--- app.py
+++ app.py
@@ -376,4 +376,4 @@ def create_list_densities():
 # ---------- Exécution ----------
 
 if __name__ == "__main__":
-    application.run(host="127.0.0.1", port=80)
+    application.run(host="localhost", port=80)
